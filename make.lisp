d0cad577e12db3eb196a532fe0c0e05a97ee1a91
jlas <juan.lasheras@gmail.com>
2015-08-16 14:59:02 -0400

    add asdf make script

---
+++ make.lisp
@@ -0,0 +1,11 @@
+#!/opt/local/bin/sbcl --script
+
+;; setup quicklisp
+(let ((quicklisp-init
+       (merge-pathnames "quicklisp/setup.lisp"
+                        (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+;; build system
+(asdf:operate 'asdf:program-op 'raytracer)
